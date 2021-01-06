f2554fb08b026dbe92485eb768c6b6ff67fa2699
Jian Chen <admin@chensoft.com>
2021-01-06 18:34:29 +0800

    kcp: allow interval set to any positive value

--- 3rd/kcp/ikcp.c
+++ 3rd/kcp/ikcp.c
@@ -1237,9 +1237,7 @@ int ikcp_setmtu(ikcpcb *kcp, int mtu)
 
 int ikcp_interval(ikcpcb *kcp, int interval)
 {
-	if (interval > 5000) interval = 5000;
-	else if (interval < 10) interval = 10;
-	kcp->interval = interval;
+	kcp->interval = interval >= 0 ? interval : 0;
 	return 0;
 }
 
@@ -1255,8 +1253,6 @@ int ikcp_nodelay(ikcpcb *kcp, int nodelay, int interval, int resend, int nc)
 		}
 	}
 	if (interval >= 0) {
-		if (interval > 5000) interval = 5000;
-		else if (interval < 10) interval = 10;
 		kcp->interval = interval;
 	}
 	if (resend >= 0) {
